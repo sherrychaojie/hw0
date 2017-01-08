43b7d6ba19c067930fd3ca51bb3818dc7f71b884
章超婕 <sherrychaojie@shouchous-MacBook-Pro.local>
2017-01-08 17:03:54 -0600

    hw

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -7,12 +7,12 @@ package uchidb;
 public class HW0Runner {
 
 	//TODO you likely will need to add member variable
-	
+	private static Containers<Integer, String> container = new Test<Integer, String>();
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 	public static Containers<Integer, String> getContainers() {
 		// TODO fix this function
-		return null;
+		return container;
 	}
 
 
